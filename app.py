530b04bf5216793e594be17f4999eb0f3eea108b
Irakli Chkhetia <iraklichkhetia9@gmail.com>
2024-04-12 15:30:43 +0400

    add Show Page and Librarian

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask, jsonify, render_template, redirect, url_for, request, s
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 from flask_wtf.csrf import generate_csrf, CSRFProtect
-
+from flask_bcrypt import Bcrypt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -13,21 +14,99 @@ app.config['SECRET_KEY'] = 'your_secret_key'
 db = SQLAlchemy(app)
 app.secret_key = b'_53oi3uriq9pifpff;apl'
 csrf = CSRFProtect(app)
+bcrypt = Bcrypt(app)
 
-class User(db.Model):
+
+
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    password_hash = db.Column(db.String(100), nullable=False)
     role = db.Column(db.String(20), nullable=False)
 
+    def serialize(self):
+        return {"id": self.id, "username": self.username, "role": self.role}
 
-class Book(db.Model):
+    def set_password(self, password):
+        self.password_hash = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.check_password_hash(self.password_hash, password)
+
+class Books(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     author = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(100), nullable=True)
     rented = db.Column(db.Boolean, default=False)
     rented_at = db.Column(db.DateTime)
     rented_by = db.Column(db.String(50))
+
+    def serialize(self):
+        return {"id": self.id, "username": self.title, "role": self.author}
+
+
+def not_user_auth(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        username = session.get("username")
+
+        user = Users.query.filter_by(username=username).first()
+        if not user or (user.role != "admin" and user.role != "librarian"):
+            return {"error": "unautorized access"}, 401
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+def is_admin(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        username = session.get("username")
+
+        user = Users.query.filter_by(username=username).first()
+
+        if not user or user.role != "admin":
+            return {"error": "only admins can register users"}, 401
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+def is_librarian(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        username = session.get("username")
+
+        user = Users.query.filter_by(username=username).first()
+
+        if not user or user.role != "librarian":
+            return {"error": "only admins can register users"}, 401
+
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if 'user_id' in session:
+        return redirect(url_for('index'))  # Редирект если пользователь уже вошел в систему
+
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = Users.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            session['user_id'] = user.id
+            session['username'] = user.username  
+            session['role'] = user.role
+            flash('Login successful.', 'success')
+            print("Username in session:", session['username'])  
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password. Please try again.', 'danger')
+            return redirect(url_for('login'))
+    return render_template('login.html')
 
 @app.route('/')
 def index():
@@ -35,10 +114,9 @@ def index():
         
         user_id = session['user_id']
         
-        users = User.query.all()
-        books = Book.query.all()
+        users = Users.query.all()
+        books = Books.query.all()
         
-       
         return render_template('index.html', users=users, books=books, user_id=user_id)
     else:
         return redirect(url_for('login'))
@@ -48,21 +126,23 @@ def search_users():
     query = request.args.get('q', '').strip().lower()
 
     if query:
-        users = User.query.filter(User.username.ilike(f'{query}%')).all()
+        users = Users.query.filter(Users.username.ilike(f'{query}%')).all()
         user_data = [{'id': user.id, 'username': user.username} for user in users]
         return jsonify(user_data)
     else:
         return jsonify([])
 
 @app.route('/create_user', methods=['GET', 'POST'])
+@not_user_auth
 def create_user():
-    if 'user_id' in session and session.get('role') == 'admin':
-        users = User.query.all()
+    if 'user_id' in session:
+        users = Users.query.all()
         if request.method == 'POST':
             username = request.form['username']
             password = request.form['password']
             role = request.form['role']
-            new_user = User(username=username, password=password, role=role)
+            new_user = Users(username=username, role=role)
+            new_user.set_password(password)
             db.session.add(new_user)
             db.session.commit()
             flash('User added successfully.', 'success')
@@ -72,9 +152,10 @@ def create_user():
         return redirect(url_for('login'))
 
 @app.route('/edit_user/<int:user_id>', methods=['GET', 'POST'])
+@not_user_auth
 def edit_user(user_id):
-    if 'user_id' in session and session.get('role') == 'admin':
-        user = User.query.get_or_404(user_id)
+    if 'user_id' in session:
+        user = Users.query.get_or_404(user_id)
         if request.method == 'POST':
             new_username = request.form['username']  
             new_password = request.form['password']  
@@ -88,56 +169,33 @@ def edit_user(user_id):
         return redirect(url_for('login'))
 
 @app.route('/delete_user/<int:user_id>', methods=['POST'])
+@not_user_auth
 def delete_user(user_id):
-    if 'user_id' in session and session.get('role') == 'admin':
-        user = User.query.get_or_404(user_id)
+    if 'user_id' in session:
+        user = Users.query.get_or_404(user_id)
         db.session.delete(user)
         db.session.commit()
         flash('User deleted successfully.', 'success')
         session.clear()
-        if not User.query.filter(User.username != 'admin').first():
+        if not Users.query.filter(Users.username != 'admin').first():
             
-            admin_user = User(username='admin', password='admin', role='admin')
+            admin_user = Users(username='admin', password='admin', role='admin')
             db.session.add(admin_user)
             db.session.commit()
             flash('Admin user created.', 'success')
         return redirect(url_for('create_user'))
     else:
         return redirect(url_for('login'))
-    
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username, password=password).first()
-        if user:
-            session['user_id'] = user.id
-            session['username'] = user.username  
-            session['role'] = user.role
-            flash('Login successful.', 'success')
-            print("Username in session:", session['username'])  
-            return redirect(url_for('index'))
-        else:
-            flash('Invalid username or password. Please try again.', 'danger')
-            return redirect(url_for('login'))
-    return render_template('login.html')
-
-
-@app.route('/logout')
-def logout():
-    session.clear()
-    session.pop('user_id', None)
-    return redirect(url_for('login'))
 
 
 @app.route('/add_book', methods=['GET', 'POST'])
+@is_librarian
 def add_book():
     if 'user_id' in session:
         if request.method == 'POST':
             title = request.form['title']
             author = request.form['author']
-            new_book = Book(title=title, author=author)
+            new_book = Books(title=title, author=author)
             db.session.add(new_book)
             db.session.commit()
             flash('Book added successfully.', 'success')
@@ -147,9 +205,10 @@ def add_book():
         return redirect(url_for('login'))
 
 @app.route('/edit_book/<int:book_id>', methods=['GET', 'POST'])
+@is_librarian
 def edit_book(book_id):
-    if 'user_id' in session and session.get('role') == 'admin':
-        book = Book.query.get_or_404(book_id)
+    if 'user_id' in session:
+        book = Books.query.get_or_404(book_id)
         if request.method == 'POST':
             if 'title' in request.form and 'author' in request.form:
                 new_title = request.form['title']
@@ -167,9 +226,10 @@ def edit_book(book_id):
 
 
 @app.route('/delete_book/<int:book_id>', methods=['POST'])
+@is_librarian
 def delete_book(book_id):
     if 'user_id' in session:
-        book = Book.query.get_or_404(book_id)
+        book = Books.query.get_or_404(book_id)
         db.session.delete(book)
         db.session.commit()
         flash('Book deleted successfully.', 'success')
@@ -177,14 +237,14 @@ def delete_book(book_id):
     else:
         return redirect(url_for('login'))
 
-
 @app.route('/assign_book/<int:book_id>', methods=['POST'])
+@is_librarian
 def assign_book(book_id):
-    if 'user_id' in session and session.get('role') == 'admin': 
+    if 'user_id' in session and session.get('role') == 'librarian': 
         user_id = request.form.get('selected_user_id')
-        book = Book.query.get_or_404(book_id) 
+        book = Books.query.get_or_404(book_id) 
         if not book.rented:
-            user = User.query.get_or_404(user_id)
+            user = Users.query.get_or_404(user_id)
             book.rented = True
             book.rented_by = user.username
             book.rented_at = datetime.now()
@@ -197,9 +257,10 @@ def assign_book(book_id):
     return redirect(url_for('index')) 
 
 @app.route('/return_book/<int:book_id>', methods=['POST'])
+@is_librarian
 def return_book(book_id):
-    if 'user_id' in session and session.get('role') == 'admin': 
-        book = Book.query.get_or_404(book_id) 
+    if 'user_id' in session and session.get('role') == 'librarian': 
+        book = Books.query.get_or_404(book_id) 
         if book.rented:
             book.rented = False
             book.rented_by = None
@@ -212,14 +273,37 @@ def return_book(book_id):
         flash('You are not authorized to perform this action.', 'danger')
     return redirect(url_for('index')) 
 
+@app.route('/show_info/<int:book_id>', methods=['GET'])
+def show_info(book_id):
+    book = Books.query.get(book_id)
+    if not book:
+        return {"error" : "book not found"}, 404
+
+    rented_by = None
+    rented_at = None
+    if book.rented:
+        user_id = book.rented_by  
+        rented_by = user_id
+        rented_at = book.rented_at.strftime('%Y-%m-%d %H:%M:%S')
+
+    return render_template('show_book.html', book=book, rented_by=rented_by, rented_at=rented_at)
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
 with app.app_context():
     db.create_all()
 
-    if not User.query.filter_by(role='admin').first():
+    if not Users.query.filter_by(role='admin').first():
         admin_username = 'admin'
         admin_password = 'admin'  
         admin_role = 'admin'
-        admin_user = User(username=admin_username, password=admin_password, role=admin_role)
+        admin_user = Users(username=admin_username, role=admin_role)
+        admin_user.set_password(admin_password)
         db.session.add(admin_user)
         db.session.commit()
         print("Admin user created.")
